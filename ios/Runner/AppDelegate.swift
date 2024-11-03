8dae970e031ce600c81de6556cdac59f62092398
Badaruddin Shaikh <the.badaruddinshaikh@gmail.com>
2024-11-03 01:20:31 -0500

    support for map

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
-import Flutter
 import UIKit
+import Flutter
+import GoogleMaps  // Import Google Maps SDK
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,10 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    // Provide the Google Maps API key
+    GMSServices.provideAPIKey("AIzaSyBPc5Z8Rq5cYI8KrBHAtT-8QDSGlx4pzo0")
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
